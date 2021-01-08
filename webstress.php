095ccfcbc3a79ae7038d1a3b64f1a255dd1fec4a
suminhong <sci97@naver.com>
2021-01-08 10:32:44 +0900

    asg

---
+++ webstress.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Stress Test</title>
+	</head>
+	<body>
+		<?php
+			$stress = $_GET["stress"];
+			if (strlen($stress) > 0) {
+				if ($stress == "start") {
+					echo("<h2>Generating Load</h2>");
+					exec("stress --cpu 4 --io 1 --vm 1 --vm-bytes 128M");
+				} elseif ($stress == "stop") {
+					echo("<h2>Killed stress</h2>");
+					exec("kill -9 (pidof stress)");
+				} else {}
+			}
+		?>
+		<center>
+			<h2>Generate Load</h2>
+			<table>
+				<tr>
+					<td>
+						<form action="index.php">
+							<input type="hidden" name="stress" value="start" />
+							<input type="submit" name="Start Stress" />
+						</form>
+					</td>
+					<td>
+						<form>
+							<input type="hidden" name="stress" value="stop" />
+							<input type="submit" name="Stop Stress" />
+						</form>
+					</td>
+				</tr>
+			</table>
+		</center>
+	</body>
+</html>
